e248f2f57118c42c20d322b4495c9c6c51bff8c1
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-01-15 12:31:01 -0500

    calculateBonus test

--- src/test/java/TestingClass.java
+++ src/test/java/TestingClass.java
@@ -1,7 +1,13 @@
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 import espol.sd.Calculation;
 import espol.sd.Employee;
 import espol.sd.EmployeeType;
 import org.junit.Test;
+import org.powermock.api.mockito.PowerMockito;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,35 +27,54 @@ public class TestingClass {
 
     @Test
     public void testCS() {
-        // Months
-        
-
         // Caso 1
         Employee employee1 = new Employee(5, "USD", 20, EmployeeType.Worker);
         float result1 = employee1.cs();
+        assertEquals(69.33F, result1, 0.01);
 
         // Caso 2
         Employee employee2 = new Employee(10, "USD", 30, EmployeeType.Supervisor);
         float result2 = employee2.cs();
+        assertEquals(84.83F, result2, 0.01);
 
         // Caso 3
         Employee employee3 = new Employee(12, "USD", 40, EmployeeType.Manager);
         float result3 = employee3.cs();
+        assertEquals(104.33F, result3, 0.01);
 
         // Caso 4
         Employee employee4 = new Employee(30, "EUR", 50, EmployeeType.Worker);
         float result4 = employee4.cs();
+        assertEquals(92.83F, result4, 0.01);
 
         // Caso 5
         Employee employee5 = new Employee(15, "USD", 30, EmployeeType.Worker);
         float result5 = employee5.cs();
+        assertEquals(31.08F, result5, 0.01);
 
         // Caso 6
         Employee employee6 = new Employee(30, "USD", 60, EmployeeType.Supervisor);
         float result6 = employee6.cs();
+        assertEquals(67.08F, result6, 0.01);
 
         // Caso 7
         Employee employee7 = new Employee(20, "USD", 70, EmployeeType.Manager);
         float result7 = employee7.cs();
+        assertEquals(85.08F, result7, 0.01);
+    }
+
+    @Test
+    public void testCalculateYearBonus() {
+        Employee worker = new Employee(20F, "USD", 20, EmployeeType.Worker);
+        assertEquals(386.00F, worker.CalculateYearBonus(), 0.01);
+
+        Employee supervisor = new Employee(55F, "USD", 20, EmployeeType.Supervisor);
+        assertEquals(248.00F, supervisor.CalculateYearBonus(), 0.01);
+
+        Employee managerUSD = new Employee(70F, "USD", 20, EmployeeType.Manager);
+        assertEquals(456.00F, managerUSD.CalculateYearBonus(), 0.01);
+
+        Employee managerEUR = new Employee(85F, "EUR", 20, EmployeeType.Manager);
+        assertEquals(466.75F, managerEUR.CalculateYearBonus(), 0.01);
     }
 }
